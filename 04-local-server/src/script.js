7d9e1e7d8a5e2fedf6b6c7e96a33209b0f9c626b
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-05-15 22:39:13 +0200

    Added the basic scene

---
+++ 04-local-server/src/script.js
@@ -0,0 +1,40 @@
+// Import Three.js
+import * as THREE from "three";
+
+// All the core classes available:
+console.log(THREE);
+
+// Basic scene from the folder 03
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 4;
+camera.position.y = 1;
+camera.position.x = 1;
+
+scene.add(camera);
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
