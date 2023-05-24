38b30a1024e20da5da3967b508c2180ed90bb8d0
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-05-24 14:27:58 +0200

    Implemented dynamic resizing and fullscreen
    - Enabled dynamic resizing of the canvas to match the window dimensions using the resize event listener.
    - Updated the camera aspect ratio, projection matrix, and renderer size when the window is resized for consistent rendering.
    - Added fullscreen functionality on double-click using the requestFullscreen() and exitFullscreen() methods to provide a fullscreen experience.
    - Applied CSS styling to ensure the canvas is positioned fixed, covers the entire viewport, and set overflow: hidden to prevent scrolling.

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -22,9 +22,33 @@ scene.add(mesh);
  * Sizes
  */
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
+
+// Resize
+window.addEventListener("resize", () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Fullscreen
+window.addEventListener("dblclick", () => {
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
 
 /**
  * Camera
@@ -49,7 +73,9 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+
 renderer.setSize(sizes.width, sizes.height);
+renderer.getPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
