cd9706543a2cad80a90c1425430f02ba560079e5
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-05-25 16:16:00 +0200

    Created second object with triangle geometry

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -9,10 +9,37 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color("#ffc8dd");
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+const triangle = new THREE.BufferGeometry();
+const secondMaterial = new THREE.MeshBasicMaterial({
+  color: "#2a9d8f",
+  wireframe: true,
+});
+const secondMesh = new THREE.Mesh(triangle, secondMaterial);
+const positionsArrayTwo = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+const indicesArray = new Uint32Array([0, 1, 2]);
+const positionsAttributeTwo = new THREE.BufferAttribute(positionsArrayTwo, 3);
+const indicesAttribute = new THREE.BufferAttribute(indicesArray, 1);
+triangle.setAttribute("position", positionsAttributeTwo);
+triangle.setIndex(indicesAttribute);
+secondMesh.position.x = 4;
+scene.add(secondMesh);
+
+// Testing
+const geometry = new THREE.BufferGeometry();
+const count = 500;
+const positionsArray = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 4;
+}
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionsAttribute);
+
+const material = new THREE.MeshBasicMaterial({ color: "#2a9d8f" });
+
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -43,7 +70,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.z = 3;
+camera.position.z = 8;
 scene.add(camera);
 
 // Controls
